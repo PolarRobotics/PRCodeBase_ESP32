08486e5d3646b029b2c88d8661f674000ac7a8d1
Corbin <63320116+Tw1ZZLER@users.noreply.github.com>
2024-10-28 20:48:02 -0400

    Added extra documentation, changed motor interface, and deleted test function

--- src/Robot/Kicker.cpp
+++ src/Robot/Kicker.cpp
@@ -1,11 +1,26 @@
 #include "Kicker.h"
 
+/**
+ * @brief Kicker v2 Special Motor Control
+ * 
+ * Controls the special motor that winds the kicker arm automatically upon the robot start.
+ * 
+ * Intended behavior is for the robot to turn on, wind the arm until it hits the limit switch, and
+ * then fall back a certain number of degrees (~15 degrees for now).
+ */
 Kicker::Kicker(uint8_t kickerPin) {
   enabled = false;
   this->kickerPin = kickerPin;
-  windupMotor.setup(kickerPin);
+  windupMotor.attach(kickerPin);
 }
 
+/**
+ * @brief Manual Action
+ * 
+ * Manually control the motor using the Triangle and X (cross) buttons.
+ * Triangle for winding
+ * Cross for unwinding
+ */
 void Kicker::action() {
   // Control the motor on the kicker
   if (ps5.Triangle())
@@ -16,36 +31,54 @@ void Kicker::action() {
     stop();
 }
 
+/**
+ * @brief Enable Function
+ * 
+ * Enables the kicker for safety purposes.
+ */
 void Kicker::enable() {
   enabled = true;
 }
 
-void Kicker::test() {
-  if (enabled) {
-    windupMotor.write(-1); //clockwise
-    delay(3000);
-    windupMotor.write(0); //stop
-    delay(1000);
-    windupMotor.write(1); //counter-clockwise
-    delay(3000);
-    windupMotor.write(0); //stop
-  }
-}
-
+/**
+ * @brief Turns motor forward
+ * 
+ * Turns the motor forward by writing the windupMotor SPECBOT_1 (D18) pin to -0.5
+ */
 void Kicker::turnForward() {
   if (enabled) {
     windupMotor.write(-0.5);
   }
 }
 
+/**
+ * @brief Turns motor reverse
+ * 
+ * Turns the motor backwards by writing the windupMotor SPECBOT_1 (D18) pin to 0.5
+ */
 void Kicker::turnReverse() {
   if (enabled) {
     windupMotor.write(0.5);
   }
 }
 
+/**
+ * @brief Stop Motor
+ * 
+ * Stops the motor by writing the windupMotor SPECBOT_1 (D18) pin to 0
+ */
 void Kicker::stop() {
   if (enabled) {
     windupMotor.write(0);
   }
 }
+
+/**
+ * @brief Automatically Wind on Startup
+ * 
+ * This function will run when the kicker starts. It will automatically wind the kicker arm
+ * to a certain level until it hits the limit switch. Then it will automatically adjust to a certain
+ * degree from the zero point (where the limit switch is).
+ * 
+ * @author Corbin Hibler
+ */
--- src/Robot/Kicker.h
+++ src/Robot/Kicker.h
@@ -3,27 +3,28 @@
 #ifndef KICKER_H
 #define KICKER_H
 
+#include <MotorInterface.h>
 #include <Robot/Robot.h>
-#include <Robot/MotorControl.h>
 #include <ps5Controller.h> // ESP PS5 library, access using global instance `ps5`
 
 /**
  * @brief Kicker header file
- * @authors Andrew Nelson 
+ * @authors Andrew Nelson
  */
 class Kicker : public Robot {
-  private:
-    bool enabled; // safety feature
-    uint8_t kickerPin;
-    MotorControl windupMotor;
-  public:
-    Kicker(uint8_t kickerPin);
-    void action() override; //! robot subclass must override action
-    void enable();
-    void test(); 
-    void turnForward(); 
-    void turnReverse(); 
-    void stop(); 
+private:
+  bool enabled; // safety feature
+  uint8_t kickerPin;
+  MotorInterface windupMotor;
+
+public:
+  Kicker(uint8_t kickerPin);
+  void action() override; //! robot subclass must override action
+  void enable();
+  void test();
+  void turnForward();
+  void turnReverse();
+  void stop();
 };
 
 #endif // KICKER_H
