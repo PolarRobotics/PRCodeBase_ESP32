c72037d50615e38c38118e4fecb6ad0dab5a8a0a
Corbin <63320116+Tw1ZZLER@users.noreply.github.com>
2024-10-30 19:48:50 -0400

    Defined new functions for kicker and adjusted constructor with new parameters

--- src/Robot/Kicker.cpp
+++ src/Robot/Kicker.cpp
@@ -2,15 +2,21 @@
 
 /**
  * @brief Kicker v2 Special Motor Control
+ * @authors Andrew Nelson, Corbin Hibler
  * 
  * Controls the special motor that winds the kicker arm automatically upon the robot start.
  * 
  * Intended behavior is for the robot to turn on, wind the arm until it hits the limit switch, and
  * then fall back a certain number of degrees (~15 degrees for now).
+ * 
+ * @param kickerPin The pin of the kicker arm's motor
+ * @param limitswitchPin the pin of the kicker arm's limit switch
  */
-Kicker::Kicker(uint8_t kickerPin) {
+Kicker::Kicker(uint8_t kickerPin, uint8_t limitSwitchPin, uint8_t kickerEncoderPin) {
   enabled = false;
   this->kickerPin = kickerPin;
+  this->limitSwitchPin = limitSwitchPin;
+  this->kickerEncoderPin = kickerEncoderPin;
   windupMotor.attach(kickerPin);
 }
 
@@ -75,10 +81,55 @@ void Kicker::stop() {
 
 /**
  * @brief Automatically Wind on Startup
+ * @author Corbin Hibler
  * 
  * This function will run when the kicker starts. It will automatically wind the kicker arm
  * to a certain level until it hits the limit switch. Then it will automatically adjust to a certain
  * degree from the zero point (where the limit switch is).
  * 
- * @author Corbin Hibler
  */
+void Kicker::homeKickingArm() {
+  if(enabled) {
+    if (limitSwitchPin) {
+      stop();
+      adjustAngle(15);
+    } else {
+      windupMotor.write(0.5);
+    }
+  }
+}
+
+/**
+ * @brief Reverse Motor a Specified Number of Degrees
+ * @author Corbin Hibler
+ *  
+ * Adjusts the angle of the kicker arm from where it currently is back to a number of degrees,
+ * in the counter-clockwise direction.
+ *  
+ *        <- 90
+ *         ___
+ *    |   /   \  ^
+ *    v   | O |  | 0
+ *   180  \___/
+ *          
+ *       -> 270
+ * 
+ * @param angle The angle that you want to add to the current angle of the kicker arm.
+ */
+void Kicker::adjustAngle(int angle) {
+  if (enabled) {
+    windupMotor.write(-0.5);
+    // Wait until the correct angle is reached
+    stop();
+  }
+}
+
+/**
+ * @brief Gets Current Angle of the Motor from Encoder
+ * @author Corbin Hibler
+ * 
+ * @return The current angle that the motor is at
+ */
+uint16_t getCurrentAngle() {
+  return -1;
+}
