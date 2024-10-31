235a450d905da77d2d68a4ff9a227146a07ecfc2
Corbin <63320116+Tw1ZZLER@users.noreply.github.com>
2024-10-30 20:36:07 -0400

    Added kicker encoder pin values to constructor with #defines

--- src/Robot/Kicker.h
+++ src/Robot/Kicker.h
@@ -7,6 +7,10 @@
 #include <Robot/Robot.h>
 #include <ps5Controller.h> // ESP PS5 library, access using global instance `ps5`
 
+#define KICKER_COUNTS_PER_ENCODER_REV 11
+#define KICKER_COUNTS_PER_ARM_REV 1188
+#define KICKER_COUNTS_PER_ARM_DEGREE 3.3
+
 /**
  * @brief Kicker header file
  * @authors Andrew Nelson
@@ -14,13 +18,20 @@
 class Kicker : public Robot {
 private:
   bool enabled; // safety feature
+  u_int16_t angleZero; // angle of the limit switch
   uint8_t kickerPin;
   uint8_t limitSwitchPin;
-  uint8_t kickerEncoderPin;
+  uint8_t kickerEncoderPinA;
+  uint8_t kickerEncoderPinB;
   MotorInterface windupMotor;
 
 public:
-  Kicker(uint8_t kickerPin, u_int8_t limitSwitchPin, uint8_t kickerEncoderPin);
+  Kicker(
+    uint8_t kickerPin, 
+    u_int8_t limitSwitchPin, 
+    uint8_t kickerEncoderPinA, 
+    u_int8_t kickerEncoderPinB
+  );
   void action() override; //! robot subclass must override action
   void enable();
   void test();
--- src/main.cpp
+++ src/main.cpp
@@ -80,7 +80,7 @@ void setup() {
     // A call to drive->setupMotors (or downcast and call to override)
     // An initialization of `lights` if needed depending on the bot type
     case kicker:
-      robot = new Kicker(SPECBOT_PIN1, SPECBOT_PIN2, ENC1_CHA);
+      robot = new Kicker(SPECBOT_PIN1, SPECBOT_PIN2, ENC1_CHA, ENC1_CHB);
       drive = new Drive(kicker, driveParams);
       drive->setupMotors(M1_PIN, M2_PIN);
       break;
